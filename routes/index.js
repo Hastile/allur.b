ec1d7039788f9b9c05ec8a225aa541504ebc9603
Hastile <tmdals1466@gmail.com>
2023-03-27 18:45:11 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const { chromium } = require('playwright');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function (req, res, next) {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.google.com');
+  const title = await page.title();
+  await browser.close();
+  console.log(`Google.com title: ${title}`)
   res.render('index.html');
 });
 
