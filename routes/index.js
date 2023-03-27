8607a7bbcab6ab379ea673b229ddbc5f9e97c4dc
Hastile <tmdals1466@gmail.com>
2023-03-27 19:27:26 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: false })
- 
-nightmare
-  .goto('https://google.com')
-  .wait('.RNmpXc')
-  .evaluate(() => document.querySelector('.RNmpXc').value)
-  .end()
-  .then(console.log)
-  .catch(error => {
-    console.error('Search failed:', error)
-  })
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   res.render('index.html');
+});
+const Nightmare = require('nightmare')
+router.get('/nightmare', async function (req, res, next) {
+  const nightmare = Nightmare({ show: false })
+   
+  nightmare
+    .goto('https://google.com')
+    .wait('.RNmpXc')
+    .evaluate(() => document.querySelector('.RNmpXc').value) // I'm feeling lucky
+    .end()
+    .then(result => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch(error => {
+      console.error('Search failed:', error)
+    })
 });
 
 module.exports = router;
